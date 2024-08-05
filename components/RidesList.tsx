416836753ba0ba7bb1bc716f4265feaf92c71799
Ivan Suero <ivan.suero@kettal.es>
2024-08-05 09:47:14 +0200

    display name

--- components/RidesList.tsx
+++ components/RidesList.tsx
@@ -3,7 +3,7 @@ import RideItem from "./RideItem";
 import type { Ride, Park } from "@/types/types";
 import { getTranslator } from "@/lib/Translator";
 
-export default async function ({rides, park}: {rides: Ride[], park?: Park}) {
+export default async function RidesList ({rides, park}: {rides: Ride[], park?: Park}) {
   return (
     <section className="mb-8 w-full items-center justify-start flex flex-col gap-16 pt-8">
       <h2 className={`text-3xl h-1 font-bold text-center hidden sm:flex flex-row gap-4 justify-center items-center ${park?.id==='19' ? 'text-blue-700' : 'text-red-600'}`}>
